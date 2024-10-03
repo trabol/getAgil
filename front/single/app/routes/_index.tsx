90916936a853fc206cb7838232246b9eb564fe74
trabol <f.trabol@gmail.com>
2024-10-03 12:31:16 -0300

    init front single

---
+++ front/single/app/routes/_index.tsx
@@ -0,0 +1,23 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Welcome to
+          </h1>
+        </header>
+      </div>
+    </div>
+  );
+}
+
